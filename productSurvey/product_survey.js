ae1deecc1058104e1d74da590849dc6d52a15aa6
craigcratergames <craig_jeffcoat@yahoo.com>
2025-01-31 17:38:56 -0500

    Practice Task

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -8,6 +8,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const userExperience = document.getElementById('Experience').value;
 
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
@@ -16,6 +17,7 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperience').innerHTML = userExperience;
 
     document.getElementById('userInfo').style.display = 'block';
 
